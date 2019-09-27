c289a45783807b345f74e4c5b1b110de91ad29fb
Gurjit Kaur Grewal <744171@student.tor.loc>
2019-09-27 15:36:10 -0400

    first commit

---
+++ assignment3.js
@@ -0,0 +1,15 @@
+const sqlite3=require('sqlite3').verbose();
+
+create database a3;
+CREATE TABLE students
+(studentid VARCHAR(20)),
+studentname VARCHAR(20),
+programgroup VARCHAR(20)
+);
+
+create database a3;
+CREATE TABLE classes
+(classid VARCHAR(20),
+classname VARCHAR(20),
+room VARCHAR(20)
+);
